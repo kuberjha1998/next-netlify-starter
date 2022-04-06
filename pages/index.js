cc1147a7b52b9f0c4b02bdb5951c7bd6095ccc30
Kuber Jha <56771134+kuberjha1998@users.noreply.github.com>
2022-04-06 20:34:28 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Kuber Jha</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Kuber Jha loves aadu" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Kuber Jha loves aadu <code>pages/index.js</code>
         </p>
       </main>
 
